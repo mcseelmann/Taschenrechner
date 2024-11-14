2b4d80bb9f7d405883e45c82cc7a1420c0f61bac
DEIDUJ <johannes.dusold@systeam.de>
2024-11-14 16:18:36 +0100

    Formatierung angepasst

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ altesErgebnis = false;
 
 //Event Listener
 
-document.addEventListener("keydown",eingabe);
+document.addEventListener("keydown", eingabe);
 
 /**
  * checks user input
  */
 function eingabe() {
   //console.log(event.key, event.keyCode);
-  if(event.keyCode >= 96 && event.keyCode <= 105 || event.keyCode >= 48 && event.keyCode <= 57){
+  if (event.keyCode >= 96 && event.keyCode <= 105 || event.keyCode >= 48 && event.keyCode <= 57) {
     number(event.key);
   }
-  else if(event.keyCode >= 106 && event.keyCode <= 111 && event.keyCode!=108){
-    switch(event.keyCode){
+  else if (event.keyCode >= 106 && event.keyCode <= 111 && event.keyCode != 108) {
+    switch (event.keyCode) {
       case 106:
         multiply();
         break;
@@ -34,18 +34,18 @@ function eingabe() {
         break;
     }
   }
-  else if(event.keyCode == 13){
+  else if (event.keyCode == 13) {
     result();
   }
-  else if(event.keyCode == 8){
+  else if (event.keyCode == 8) {
     back();
-  } 
+  }
 }
 //END Event Listener
 
 //Nummer und operanden eingabe
-function number(n){
-  if(altesErgebnis == true){
+function number(n) {
+  if (altesErgebnis == true) {
     rechnung = []
     erg = "";
     zwErg = "";
@@ -64,8 +64,8 @@ function number(n){
 /**
  * function adds
  */
-function plus(){
-  if(altesErgebnis == true){
+function plus() {
+  if (altesErgebnis == true) {
     rechnung = [];
     rechnung.push(erg);
     altesErgebnis = false;
@@ -77,10 +77,10 @@ function plus(){
 }
 
 /**
- * function subtracts 
+ * function subtracts
  */
-function minus(){
-  if(altesErgebnis == true){
+function minus() {
+  if (altesErgebnis == true) {
     rechnung = [];
     rechnung.push(erg);
     altesErgebnis = false;
@@ -90,8 +90,8 @@ function minus(){
   rechnung.push(" - ");
   showRechnung(rechnung);
 }
-function multiply(){
-  if(altesErgebnis == true){
+function multiply() {
+  if (altesErgebnis == true) {
     rechnung = [];
     rechnung.push(erg);
     altesErgebnis = false;
@@ -101,8 +101,8 @@ function multiply(){
   rechnung.push(" * ");
   showRechnung(rechnung);
 }
-function divide(){
-  if(altesErgebnis == true){
+function divide() {
+  if (altesErgebnis == true) {
     rechnung = [];
     rechnung.push(erg);
     altesErgebnis = false;
@@ -112,8 +112,8 @@ function divide(){
   rechnung.push(" / ");
   showRechnung(rechnung);
 }
-function modulo(){
-  if(altesErgebnis == true){
+function modulo() {
+  if (altesErgebnis == true) {
     rechnung = [];
     rechnung.push(erg);
     altesErgebnis = false;
@@ -123,7 +123,7 @@ function modulo(){
   rechnung.push(" % ");
   showRechnung(rechnung);
 }
-function C(){
+function C() {
   zwErg = "";
   erg = "";
   document.getElementById("erg").innerHTML = "0";
@@ -131,43 +131,43 @@ function C(){
   showRechnung(rechnung);
   altesErgebnis = false;
 }
-function CE(){
+function CE() {
   document.getElementById("erg").innerHTML = "0";
   zwErg = "";
 }
-function back(){
-  if(zwErg == 0){
+function back() {
+  if (zwErg == 0) {
     document.getElementById("erg").innerHTML = "0";
-  } else if(zwErg.length == 1){
+  } else if (zwErg.length == 1) {
     document.getElementById("erg").innerHTML = "0";
     zwErg = "";
-  } else{
-    zwErg = zwErg.substr(0, zwErg.length-1);
+  } else {
+    zwErg = zwErg.substr(0, zwErg.length - 1);
     show(zwErg);
   }
 }
-function plusminus(){
-  if(altesErgebnis == true){
+function plusminus() {
+  if (altesErgebnis == true) {
     zwErg = "" + erg;
-    if(zwErg == 0){
+    if (zwErg == 0) {
       document.getElementById("erg").innerHTML = "0";
-    } 
-    else if(zwErg > 0){
+    }
+    else if (zwErg > 0) {
       rechnung = [];
       showRechnung(rechnung);
       zwHelp = "";
       zwHelp = "-" + zwErg.substring(0);
       show(zwHelp);
-    } 
+    }
     else {
       zwErg = "" + zwErg.substring(1);
       show(zwErg);
     }
     altesErgebnis = false;
-    }
-  if(zwErg == 0){
+  }
+  if (zwErg == 0) {
     document.getElementById("erg").innerHTML = "0";
-  } else if(zwErg > 0){
+  } else if (zwErg > 0) {
     zwErg = "-" + zwErg.substring(0);
     show(zwErg);
   } else {
@@ -175,25 +175,25 @@ function plusminus(){
     show(zwErg);
   }
 }
-function sqrt(){
+function sqrt() {
   rechnung.push(Math.sqrt(zwErg));
-  zwErg = ""; 
+  zwErg = "";
   showRechnung(rechnung);
 }
-function sqr(){
+function sqr() {
   let i = zwErg * zwErg;
   zwErg = "";
   rechnung.push(i);
   showRechnung(rechnung);
 }
-function einsDurchx(){
+function einsDurchx() {
   rechnung.push(" 1/");
   rechnung.push(zwErg);
   zwErg = "";
   showRechnung(rechnung);
-  
+
 }
-function cube(){
+function cube() {
   let i = zwErg * zwErg * zwErg;
   zwErg = "";
   rechnung.push(i);
@@ -202,9 +202,9 @@ function cube(){
 //END nummer und operanden eingabe
 
 //Ergebnis und Anzeigemethoden
-function result(){
-  if(rechnung.length != 0){
-    rechnung.push(zwErg); 
+function result() {
+  if (rechnung.length != 0) {
+    rechnung.push(zwErg);
     showRechnung(rechnung);
     erg = eval(rechnung.join(""));
     show(erg);
@@ -212,72 +212,71 @@ function result(){
     altesErgebnis = true;
     //console.log(erg + "/" + zwErg + "/" +rechnung + "/" + altesErgebnis);
   }
-  
 }
 
-function show(e){
+function show(e) {
   document.getElementById("erg").innerHTML = e;
 }
 
-function showRechnung(r){
+function showRechnung(r) {
   document.getElementById("rechnung").innerHTML = r.join("");
 }
 //END Ergebnis und Anzeigemethoden
-  
-  
-//Verlaufsspeicherung 
+
+
+//Verlaufsspeicherung
 let v1 = [];
 let v2 = [];
 let v3 = [];
 let v4 = [];
 
-function verlaufSpeichern(rech, ergeb){
+function verlaufSpeichern(rech, ergeb) {
   let i = 0;
-  if(v1.length<1){
-    while(i < rechnung.length){
-    v1[i] = rechnung[i];
-    i++;
+  if (v1.length < 1) {
+    while (i < rechnung.length) {
+      v1[i] = rechnung[i];
+      i++;
     }
     v1.push(" = ");
     v1.push("<b>" + erg + "</b>");
     zwErg = "";
     document.getElementById("verlauf1").style.display = "block";
     document.getElementById("verlauf1").innerHTML = v1.join("");
-  } 
-  else if(v2.length<1){
-    while(i < rechnung.length){
-    v2[i] = rechnung[i];
-    i++;
+  }
+  else if (v2.length < 1) {
+    while (i < rechnung.length) {
+      v2[i] = rechnung[i];
+      i++;
     }
     v2.push(" = ");
     v2.push("<b>" + erg + "</b>");
     zwErg = "";
     document.getElementById("verlauf2").style.display = "block";
     document.getElementById("verlauf2").innerHTML = v2.join("");
-  } 
-  else if(v3.length<1){
-    while(i < rechnung.length){
-    v3[i] = rechnung[i];
-    i++;
+  }
+  else if (v3.length < 1) {
+    while (i < rechnung.length) {
+      v3[i] = rechnung[i];
+      i++;
     }
     v3.push(" = ");
     v3.push("<b>" + erg + "</b>");
     zwErg = "";
     document.getElementById("verlauf3").style.display = "block";
     document.getElementById("verlauf3").innerHTML = v3.join("");
-  } 
-  else if(v4.length<1){
-    while(i < rechnung.length){
-    v4[i] = rechnung[i];
-    i++;
+  }
+  else if (v4.length < 1) {
+    while (i < rechnung.length) {
+      v4[i] = rechnung[i];
+      i++;
     }
     v4.push(" = ");
     v4.push("<b>" + erg + "</b>");
     zwErg = "";
     document.getElementById("verlauf4").style.display = "block";
     document.getElementById("verlauf4").innerHTML = v4.join("");
-  } 
-  else{
+  }
+  else {
     v1 = v2;
     v2 = v3;
     v3 = v4;
@@ -288,44 +287,44 @@ function verlaufSpeichern(rech, ergeb){
     verlaufSpeichern(rech, ergeb)
   }
   showRechnung(rechnung);
-  
+
 }
 
-function abrufen(p){
+function abrufen(p) {
   let i = 0;
   rechnung = [];
-  switch(p){
+  switch (p) {
     case 4:
-      while(i<v4.length-2){
-      rechnung[i] = v4[i];
-      i++;
+      while (i < v4.length - 2) {
+        rechnung[i] = v4[i];
+        i++;
       }
-      erg = v4[v4.length-1];
-      erg = erg.slice(3,erg.length-4);
+      erg = v4[v4.length - 1];
+      erg = erg.slice(3, erg.length - 4);
       break;
     case 3:
-      while(i<v3.length-2){
-      rechnung[i] = v3[i];
-      i++;
+      while (i < v3.length - 2) {
+        rechnung[i] = v3[i];
+        i++;
       }
-      erg = v3[v3.length-1];
-      erg = erg.slice(3,erg.length-4);
+      erg = v3[v3.length - 1];
+      erg = erg.slice(3, erg.length - 4);
       break;
     case 2:
-      while(i<v2.length-2){
-      rechnung[i] = v2[i];
-      i++;
+      while (i < v2.length - 2) {
+        rechnung[i] = v2[i];
+        i++;
       }
-      erg = v2[v2.length-1];
-      erg = erg.slice(3,erg.length-4);
+      erg = v2[v2.length - 1];
+      erg = erg.slice(3, erg.length - 4);
       break;
     case 1:
-      while(i<v1.length-2){
-      rechnung[i] = v1[i];
-      i++;
+      while (i < v1.length - 2) {
+        rechnung[i] = v1[i];
+        i++;
       }
-      erg = v1[v1.length-1];
-      erg = erg.slice(3,erg.length-4);
+      erg = v1[v1.length - 1];
+      erg = erg.slice(3, erg.length - 4);
       break;
   }
   showRechnung(rechnung);
@@ -333,13 +332,13 @@ function abrufen(p){
   altesErgebnis = true;
 }
 
-//END Verlaufsspeicherung 
+//END Verlaufsspeicherung
 
 let aindex = 0;
-function speicherSpeichern(){
+function speicherSpeichern() {
   storage = JSON.parse(localStorage.getItem("storage"));
-  if(zwErg != 0){
-    switch(storage.length){
+  if (zwErg != 0) {
+    switch (storage.length) {
       case 0:
         storage[0] = zwErg;
         break;
@@ -365,8 +364,8 @@ function speicherSpeichern(){
         storage[0] = zwErg;
     }
   }
-  if(erg != 0){
-    switch(storage.length){
+  if (erg != 0) {
+    switch (storage.length) {
       case 0:
         storage[0] = erg.toString();
         break;
@@ -400,43 +399,43 @@ function speicherSpeichern(){
 
 //console.log(erg + "/" + zwErg + "/" +rechnung + "/" + altesErgebnis);
 showSpeicher();
-function showSpeicher(){
-  if(localStorage.getItem("storage") == '""'){
+function showSpeicher() {
+  if (localStorage.getItem("storage") == '""') {
     storage = [];
     localStorage.setItem("storage", JSON.stringify(storage));
   }
   zwHelp = JSON.parse(localStorage.getItem("storage"));
-  if(zwHelp.length == 0){
+  if (zwHelp.length == 0) {
     document.getElementById("speicherBox1").style.display = "none";
     document.getElementById("speicherBox2").style.display = "none";
     document.getElementById("speicherBox3").style.display = "none";
     document.getElementById("speicherBox4").style.display = "none";
   }
-  else{
+  else {
     document.getElementById("speicher1").innerHTML = zwHelp[0];
     document.getElementById("speicherBox1").style.display = "block";
-    
-    if(zwHelp.length == 1){
+
+    if (zwHelp.length == 1) {
       document.getElementById("speicherBox2").style.display = "none";
       document.getElementById("speicherBox3").style.display = "none";
       document.getElementById("speicherBox4").style.display = "none";
     }
-    else{
+    else {
       document.getElementById("speicher2").innerHTML = zwHelp[1];
       document.getElementById("speicherBox2").style.display = "block";
-      
-      if(zwHelp.length == 2){
+
+      if (zwHelp.length == 2) {
         document.getElementById("speicherBox3").style.display = "none";
         document.getElementById("speicherBox4").style.display = "none";
       }
-      else{
+      else {
         document.getElementById("speicher3").innerHTML = zwHelp[2];
         document.getElementById("speicherBox3").style.display = "block";
-        
-        if(zwHelp.length == 3){
+
+        if (zwHelp.length == 3) {
           document.getElementById("speicherBox4").style.display = "none";
         }
-        else{
+        else {
           document.getElementById("speicher4").innerHTML = zwHelp[3];
           document.getElementById("speicherBox4").style.display = "block";
         }
@@ -445,9 +444,9 @@ function showSpeicher(){
   }
 }
 
-function wiederHolen(id){
+function wiederHolen(id) {
   zwHelp = JSON.parse(localStorage.getItem("storage"));
-  switch(id){
+  switch (id) {
     case 2:
       erg = zwHelp[1];
       break;
@@ -467,13 +466,13 @@ function wiederHolen(id){
   altesErgebnis = true;
 }
 
-function loeschen(id){
+function loeschen(id) {
   //console.log(id);
   zwHelp = JSON.parse(localStorage.getItem("storage"));
-  switch(id){
+  switch (id) {
     case 1:
 
-      zwHelp =  zwHelp.slice(1,4);
+      zwHelp = zwHelp.slice(1, 4);
       break;
     case 2:
       zwHelp = zwHelp[0] + zwHelp.slice(2);
@@ -492,17 +491,17 @@ function loeschen(id){
   showSpeicher();
 }
 
-function memoryPlus(id){
+function memoryPlus(id) {
   //console.log(id);
   zwHelp = JSON.parse(localStorage.getItem("storage"));
   wert = 0;
-  if(erg != 0){
+  if (erg != 0) {
     wert = erg;
-  } 
-  if(zwErg != 0){
+  }
+  if (zwErg != 0) {
     wert = parseFloat(zwErg);
   }
-  switch(id){
+  switch (id) {
     case 1:
       zwHelp[0] = parseFloat(zwHelp[0]) + wert;
       break;
@@ -522,16 +521,16 @@ function memoryPlus(id){
   showSpeicher();
 }
 
-function memoryMinus(id){
+function memoryMinus(id) {
   zwHelp = JSON.parse(localStorage.getItem("storage"));
   wert = 0;
-  if(erg != 0){
+  if (erg != 0) {
     wert = erg;
-  } 
-  if(zwErg != 0){
+  }
+  if (zwErg != 0) {
     wert = parseFloat(zwErg);
   }
-  switch(id){
+  switch (id) {
     case 1:
       zwHelp[0] = parseFloat(zwHelp[0]) - wert;
       break;
